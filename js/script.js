1f4697693759147ae428ac9363b834f67418a885
bentleycodes <brandonprasouvo@live.com>
2022-06-30 17:01:49 -0700

    day two challenge

--- js/script.js
+++ js/script.js
@@ -9,4 +9,13 @@ console.log("Bentley");
 console.log("Ice cream");
 
 
+const string = "Supercalifragilisticexpialidocious";
+console.log(string.length);
+
+const number = 1;
+const bigint = 5n;
+console.log(number);
+console.log(bigint);
+console.log(typeof(number));
+console.log(typeof(bigint));
 
